0226175f771d7dfb14de14bb68eb8e5066ca0cf8
Alexandru Craciun <ax.craciun@gmail.com>
2014-03-22 11:24:40 +0100

    added checkresult examples

--- default-examples/src/main/java/org/bugby/bugs/pmd/CheckResultSetNextTrigger.java
+++ default-examples/src/main/java/org/bugby/bugs/pmd/CheckResultSetNextTrigger.java
@@ -3,15 +3,13 @@ package org.bugby.bugs.pmd;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.bugby.annotation.GoodExample;
+import org.bugby.annotation.GoodExampleTrigger;
 
-@GoodExample
-public class CheckResultSetNext {
+@GoodExampleTrigger(forExample = CheckResultSetNext.class)
+public class CheckResultSetNextTrigger {
 	// TODO i should tell to ignore the throws
 	public void someCode(ResultSet someVar) throws SQLException {
 		// TODO i should make sure this does not match if (set.next()) or while(set.next())
-		while (someVar.next()) {
-
-		}
+		someVar.next();
 	}
 }
